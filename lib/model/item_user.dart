0783a3db6d2d8636b754c839ef165adbd63f977e
LeeSeulBee <slashb410@gmail.com>
2023-01-04 16:34:18 +0900

    Fix: Model 파이어베이스와 통일

--- lib/model/item_user.dart
+++ lib/model/item_user.dart
@@ -2,17 +2,20 @@ class UserItem {
   String? uId;
   String nickname = 'No Nickname';
   String profileUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNYfI9jg0TRgOlwhYgZaJvj_-zl8uhfpcqMw&usqp=CAU';
+  List<String> hashTag = [];
   DateTime joinDate = DateTime.now();
   DateTime lastVisitDate = DateTime.now();
 
   UserItem.fromJson(Map<String, dynamic> json)
       : uId = json['uId'],
         nickname = json['nickname'],
+        hashTag = json['hashtag'] == null ? [] : json['hashtag'] as List<String>,
         profileUrl = json['profileUrl'] ?? 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNYfI9jg0TRgOlwhYgZaJvj_-zl8uhfpcqMw&usqp=CAU';
 
   Map<String, dynamic> toJson() => <String, dynamic> {
     'uId' : uId,
     'nickname': nickname,
-    'profileUrl' : profileUrl
+    'profileUrl' : profileUrl,
+    'hashtag' : hashTag
   };
 }
