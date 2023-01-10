3430be7643a92ef87dff4db332bb165a640415cc
Nyeong <iwbm312@gmail.com>
2023-01-10 18:46:52 +0900

    Design: 홈화면 ui변경

--- lib/page/page_home.dart
+++ lib/page/page_home.dart
@@ -36,26 +36,37 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(child: Center(
       child: SizedBox(
-        height: 200,
+        height: 250,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            const Text('이름을 검색하여\n다른 사람들의 위시리스트를\n확인 할 수 있어요\n\n', textAlign: TextAlign.center, style:TextStyle(fontSize: 40)),
-            GestureDetector(
-                onTap: () => goSearchPage(),
-                child: Container(
-                  height: 50,
-                  decoration : BoxDecoration(
-                    boxShadow: <BoxShadow>[
-                      BoxShadow(
-                          color: Theme.of(context).shadowColor.withOpacity(0.3),
-                    blurRadius: 5, offset: const Offset(0,3),
-                      ),
-                    ],
-                    borderRadius: const BorderRadius.all(Radius.circular(15.0)),
-                    color: Colors.white,
+            const Text('이름을 검색하여\n다른 사람들의 위시리스트를\n확인할 수 있어요',
+                textAlign: TextAlign.center,
+                style:TextStyle(fontSize: 30)),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30, 30, 30, 0),
+              child: GestureDetector(
+                  onTap: () => goSearchPage(),
+                  child: Container(
+                    height: 50,
+                    child: Row(
+                      children: [
+                        IconButton(onPressed: (){}, icon: const Icon(Icons.search)),
+                        Spacer(),
+                      ],
+                    ),
+                    decoration : BoxDecoration(
+                      boxShadow: <BoxShadow>[
+                        BoxShadow(
+                            color: Theme.of(context).shadowColor.withOpacity(0.3),
+                      blurRadius: 5, offset: const Offset(0,3),
+                        ),
+                      ],
+                      borderRadius: const BorderRadius.all(Radius.circular(15.0)),
+                      color: Colors.grey[300],
+                    ),
                   ),
-                ),
+              ),
             ),
         ],
         ),
